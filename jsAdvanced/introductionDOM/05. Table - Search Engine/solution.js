fd67ccb11acfbea046177a3f46b6c695c8d88b21
KalinHar <kalix@abv.bg>
2021-09-24 21:32:34 +0300

    new

--- jsAdvanced/introductionDOM/05. Table - Search Engine/solution.js
+++ jsAdvanced/introductionDOM/05. Table - Search Engine/solution.js
@@ -2,7 +2,16 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
-
+      let rows = Array.from(document.querySelectorAll('tbody tr'));
+      let match = document.getElementById('searchField').value.toLowerCase();
+      
+      rows.forEach(element => {     
+         if (element.textContent.toLocaleLowerCase().includes(match)) {
+            element.classList.add("select");
+         }else {
+            element.classList.remove("select");
+         }
+      });
+      document.getElementById('searchField').value = ''
    }
 }
