b0939f62561999307bb65775c33d765c039d82c5
KalinHar <kalix@abv.bg>
2021-09-26 10:25:48 +0300

    new

--- jsAdvanced/introductionDOM/08. Generate Report/report.js
+++ jsAdvanced/introductionDOM/08. Generate Report/report.js
@@ -1,3 +1,20 @@
 function generateReport() {
-    //TODO
+    let report = [];
+    let elementsInput = Array.from(document.getElementsByTagName('input'));
+    let rows = Array.from(document.getElementsByTagName('tr')).splice(1);
+    let checkedCols = [];
+    for (let i=0; i<elementsInput.length; i++) {
+        if (elementsInput[i].checked) {
+            checkedCols.push(i);
+        }
+    }
+    for (let row of rows) {
+        let result = {};
+        for (let c of checkedCols) {
+            result[elementsInput[c].name] = row.children[c].textContent;
+        }
+        report.push(result);
+    }
+    // console.log(JSON.stringify(report));
+    document.querySelector('#output').innerHTML = JSON.stringify(report);
 }
