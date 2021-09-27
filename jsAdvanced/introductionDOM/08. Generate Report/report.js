135e512afaca1df351d8c58d2d7ef42e2be13544
KalinHar <kalix@abv.bg>
2021-09-27 12:24:18 +0300

    new

--- jsAdvanced/introductionDOM/08. Generate Report/report.js
+++ jsAdvanced/introductionDOM/08. Generate Report/report.js
@@ -2,16 +2,12 @@ function generateReport() {
     let report = [];
     let elementsInput = Array.from(document.getElementsByTagName('input'));
     let rows = Array.from(document.getElementsByTagName('tr')).splice(1);
-    let checkedCols = [];
-    for (let i=0; i<elementsInput.length; i++) {
-        if (elementsInput[i].checked) {
-            checkedCols.push(i);
-        }
-    }
+    let checkedCols = elementsInput.map((el, i) => [el, i]).filter(el =>  el[0].checked);
+    
     for (let row of rows) {
         let result = {};
-        for (let c of checkedCols) {
-            result[elementsInput[c].name] = row.children[c].textContent;
+        for (let [el, i] of checkedCols) {
+            result[el.name] = row.children[i].textContent;
         }
         report.push(result);
     }
