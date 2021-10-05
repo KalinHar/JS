198c6e9b1b82ae8e4293a2c3d66ec509debf10ed
KalinHar <kalix@abv.bg>
2021-10-05 19:07:42 +0300

    new

---
+++ jsAdvanced/test/requestValidator01.js
@@ -0,0 +1,34 @@
+function rValidator(input) {
+    
+    const validMethod = ['GET', 'POST', 'DELETE', 'CONNECT'];
+    const validVersion = ['HTTP/0.9', 'HTTP/1.0', 'HTTP/1.1', 'HTTP/2.0'];
+    // const unvalidMessageSymb = ['<', '>', '\\', '&', '"', '\''];
+    const uriRegex = /(^[\w.]+$)/;
+    const msgRegex = /([<>\\&'"])/;
+    if (!input.method || !validMethod.includes(input.method)) {
+        throw new Error('Invalid request header: Invalid Method');
+    }
+
+    if (!input.uri || input.uri == '' || !uriRegex.test(input.uri)) {
+        throw new Error('Invalid request header: Invalid URI');
+    }
+
+    if (!input.version || !validVersion.includes(input.version)) {
+        throw new Error('Invalid request header: Invalid Version');
+    }
+
+    if (input.message == undefined || msgRegex.test(input.message)) {
+        throw new Error('Invalid request header: Invalid Message');
+    }
+    return input;
+}
+
+
+toValidate = {
+    method: 'GET',
+    uri: 'svn.public.catalog',
+    version: 'HTTP/1.1',
+    message: ''
+}
+
+console.log(rValidator(toValidate));
