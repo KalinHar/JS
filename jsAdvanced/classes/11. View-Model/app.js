2d0ecf65322782240018cf8a4ebe34521dab419f
KalinHar <kalix@abv.bg>
2021-10-09 21:01:22 +0300

    new

--- jsAdvanced/classes/11. View-Model/app.js
+++ jsAdvanced/classes/11. View-Model/app.js
@@ -1,8 +1,36 @@
 class Textbox {
-    //TODO
+    constructor(selector, regex) {
+        this._value = '';
+        this._elements = document.querySelectorAll(selector);
+        this._invalidSymbols = regex;
+    }
+
+    get value() {
+        return this._value;
+    }
+    set value(val) {
+        this._value = val;
+        for (let el of this.elements) {
+            el.value = val;
+        }
+    }
+
+    get elements() {
+        return this._elements;
+    }
+
+    isValid() {
+        return !this._invalidSymbols.test(this.value);
+    }
 }
 
 let textbox = new Textbox(".textbox",/[^a-zA-Z0-9]/);
-let inputs = document.getElementsByClassName('.textbox');
-
-inputs.addEventListener('click',function(){console.log(textbox.value);});
+let inputs = document.getElementsByClassName('textbox');
+console.log(inputs)
+// textbox.value = "Tesst";
+console.log(inputs[0].value == 'Tesst');
+console.log(inputs[1].value == 'Tesst');
+// textbox.value = "asd"
+// console.log(textbox.isValid())
+// console.log(textbox._invalidSymbols)
+// inputs.addEventListener('click',function(){console.log(textbox.value);});
