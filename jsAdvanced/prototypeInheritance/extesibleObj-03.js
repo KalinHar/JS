32cfd859c953a81ef48b8be9aa64cffe8fe49b50
KalinHar <kalix@abv.bg>
2021-10-12 21:35:55 +0300

    new

--- jsAdvanced/prototypeInheritance/extesibleObj-03.js
+++ jsAdvanced/prototypeInheritance/extesibleObj-03.js
@@ -1,11 +1,19 @@
 function extensibleObject() {
-    function extend(temp) {
-        
-    }
-    return {
-        extend
-    }
+    let obj = {
+        extend: function(template){
+            for(let [key, value] of Object.entries(template)){
+                if(typeof(value) == "function"){
+                    Object.getPrototypeOf(obj)[key] = value;
+                } else {
+                    obj[key] = value;
+                }
+            }
+        }
+    };
+
+    return obj;
 }
+
 const myObj = extensibleObject();
 
 const template = {
--- jsAdvanced/prototypeInheritance/stringExt-02.js
+++ jsAdvanced/prototypeInheritance/stringExt-02.js
@@ -16,44 +16,58 @@
         return this.length == 0;
     };
     String.prototype.truncate = function (n) {
-        if (this.length <= n) {
-            return `${this}`;
+        if (n <= 3) {
+            return ".".repeat(n);
         }
-        let res = this.slice(0, n);
-        res = res.split(' ');
-        if (res.length > 1) {
-            res.pop();
-            return res.join(' ') + '...';
+        if (this.toString().length <= n) {
+            return this.toString();
+        } else {
+            let lastIndex = this.toString().substr(0, n - 2).lastIndexOf(" ");
+            if (lastIndex != -1) {
+                return this.toString().substr(0, lastIndex) + "...";
+            } else {
+                return this.toString().substr(0, n - 3) + "...";
+            }
         }
-        if (n >= 4) {
-            res = res[0].slice(0, n - 3);
-            return res + '...';
-        }
-        return '.'.repeat(n);
+        // if (this.length <= n) {
+        //     return `${this}`;
+        // }
+        // let res = this.slice(0, n);
+        // res = res.split(' ');
+        // console.log(res);
+        // if (res.length > 1) {
+        //     res.pop();
+        //     return res.join(' ') + '...';
+        // }
+        // if (n >= 4) {
+        //     res = res[0].slice(0, n - 3);
+        //     return res + '...';
+        // }
+        // return '.'.repeat(n);
     };
-    
-    // String.prototype.format = function (string, ...params) {
-    //     let words = params;
-    //     let regex = /{\d+}/;
-    //     while (regex.test(string) && words.length != 0) {
-    //         string = string.replace(regex, words.shift())
-    //     }
-    //     return string;
-    // };
-    
+
+    String.format = function (string, ...params) {
+        let words = params;
+        let regex = /{\d+}/;
+        while (regex.test(string) && words.length != 0) {
+            string = string.replace(regex, words.shift())
+        }
+        return string;
+    };
+
 })();
 
 let str = 'my string';
 str = str.ensureStart('my');
 str = str.ensureStart('hello ');
-str = str.truncate(16);
-str = str.truncate(14);
+// str = str.truncate(16);
+// str = str.truncate(9);
+str = str.truncate(6);
 console.log(str);
-str = str.truncate(8);
 str = str.truncate(4);
 str = str.truncate(2);
 str = String.format('The {0} {1} fox',
-  'quick', 'brown');
+    'quick', 'brown');
 // str = String.format('jumps {0} {1}',
 //   'dog');
 
